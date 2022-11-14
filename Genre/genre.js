439ebdac97ba26e1e41b1bcba8acb02f89f5de64
unknown <caglabircan@hotmail.com>
2022-11-14 21:40:40 +0300

    added navbar css

--- Genre/genre.js
+++ Genre/genre.js
@@ -5,7 +5,31 @@ const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const genresNav = document.getElementById("genres")
 
+let genres = {
+  28: "Action",
+  12: "Adventure",
+  16: "Animation",
+  35: "Comedy",
+  80: "Crime",
+  99: "Documentary",
+  18: "Drama",
+  10751: "Family",
+  14: "Fantasy",
+  36: "History",
+  27: "Horror",
+  10402: "Music",
+  9648: "Mystery",
+  10749: "Romance",
+  878: "Science Fiction",
+  10770: "TV Movie",
+  53: "Thriller",
+  10752: "War",
+  37: "Western",
+};
 
+genresMenu.addEventListener("click", (e) => {
+  App.runGenres(e.target.id);
+});
 
 // generate the genres url 
 const genresUrl = (genreId) => {
