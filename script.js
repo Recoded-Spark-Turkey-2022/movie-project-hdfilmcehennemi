304293cae36ba049db3d0c58ceb3d7b19f1ad17f
Seda Ünal <sedaylmz06@outlook.com>
2022-11-15 18:13:24 +0300

    movies by genre

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
-'use strict';
+"use strict";
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
+const cardRow = document.querySelector(".row");
 
 // Don't touch this function please
 const autorun = async () => {
   const movies = await fetchMovies();
-  renderMovies(movies.results);
+  //renderMovies(movies.results);
+
+  const moviesByGenre = await fetchMoviesByGenre();
+
+  let films = movies.results.concat(moviesByGenre.results);
+  renderMovies(films);
+  /*
+  console.log(moviesByGenre.results);
+  */
 };
 
 // Don't touch this function please
 const constructUrl = (path) => {
-  return `${TMDB_BASE_URL}/${path}?api_key=${atob(
-    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
-  )}`;
+  let daa = path.split("?");
+  let ddd = [];
+  if (daa.length > 1) {
+    ddd = daa[1].split("&");
+  }
+  let param = "";
+  ddd.forEach((x, i) => {
+    if (i == 0) {
+      param += "&";
+    }
+    param += x;
+  });
+  return `${TMDB_BASE_URL}/${daa[0]}?api_key=${
+    atob("NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=") + param
+  }`;
 };
 
 // You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
+
   renderMovie(movieRes);
 };
 
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
 const fetchMovies = async () => {
-  const url = constructUrl(`movie/now_playing`);
+  const url = constructUrl(`/movie/popular`);
+  const res = await fetch(url);
+  return res.json();
+};
+
+const fetchMoviesByGenre = async (genreId) => {
+  const url = constructUrl(`discover/movie?with_genres=${genreId}`);
+  console.log(url);
   const res = await fetch(url);
   return res.json();
 };
@@ -40,17 +69,35 @@ const fetchMovie = async (movieId) => {
 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
-  movies.map((movie) => {
+  cardRow.innerHTML = "";
+  movies.forEach((movie, i) => {
+    let genres = "";
+    movie.genre_ids.forEach((genre) => {
+      genres += `${genre},`;
+    });
+
     const movieDiv = document.createElement("div");
+    movieDiv.setAttribute("data-categories", genres);
     movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-      movie.title
-    } poster">
-        <h3>${movie.title}</h3>`;
+  <div key="${i}">
+      <div class="card " >
+      <div class="card-body">
+      <img class="card-img-top" src="${
+        BACKDROP_BASE_URL + movie.backdrop_path
+      }" alt="${movie.title} poster">
+      <h4 class="card-title">vote average ${movie.vote_average}  </h4>
+      <p class="card-text">${movie.title}</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+
+    </div>
+    </div>
+    </div>
+
+          `;
     movieDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
-    CONTAINER.appendChild(movieDiv);
+    cardRow.appendChild(movieDiv);
   });
 };
 
@@ -79,3 +126,43 @@ const renderMovie = (movie) => {
 };
 
 document.addEventListener("DOMContentLoaded", autorun);
+
+window.addEventListener("load", () => {
+  const dropItems = document.querySelectorAll(".dropdown-item");
+
+  // const categories = (categoryId) => {
+  //   let result;
+  //   cardRow.childNodes.forEach((a) => {
+  //     a.dataset.categories.split(",").forEach((item) => {
+  //       if (item !== "") {
+  //         if (item == categoryId) {
+  //           result = item;
+  //         }
+  //       }
+  //     });
+  //   });
+  //   return result;
+  // };
+
+  // const displayMove = (categoryId) => {
+  //   cardRow.childNodes.forEach((item) => {
+  //     const category = item.dataset.categories.search(categoryId);
+  //     item.style.display = "block";
+  //     if (category === -1) {
+  //       // console.log(a[i])
+  //       item.style.display = "none";
+  //     }
+  //   });
+  // };
+
+  dropItems.forEach((dropItem) => {
+    dropItem.addEventListener("click", async () => {
+      const moviesByGenre = await fetchMoviesByGenre(dropItem.dataset.category);
+      console.log(moviesByGenre);
+
+      // let films = movies.results.concat(moviesByGenre.results);
+      renderMovies(moviesByGenre.results);
+      // console.log(displayMove(categories(dropItem.dataset.category)));
+    });
+  });
+});
